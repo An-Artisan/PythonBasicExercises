65fad2a6ddf8293b3b62a75632a1d294799c0b60
DESKTOP-NBP4V3D\不敢为天下 <1090035743@qq.com>
2017-06-18 01:08:19 +0800

    第七次提交： 1：GUI选择文件之后显示文件的内容

--- ShowFile.py
+++ ShowFile.py
@@ -1,2 +1,10 @@
 import easygui as g
 import  sys
+# 打开文件的窗口，只显示.py的扩展名，返回选择的文件路径
+file_name  = g.fileopenbox(default="./*py")
+# 打开该文件，设置编码为UTF-8
+file = open(file_name,'r',encoding='UTF-8')
+# 显示在box中
+g.textbox("你选择的文件内容为：","显示文件内容",text=file.readlines())
+# 关闭文件
+file.close()
